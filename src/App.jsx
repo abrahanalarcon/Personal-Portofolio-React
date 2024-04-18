6645c155a8871f422cd584c350141f1d4cd7b51b
abrahanalarcon <moiseshazim@gmail.com>
2024-04-18 13:58:42 -0400

    creatinf from and validation's functions

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from "./components/Header"
 import './App.css';
 import MainContent from "./components/MainContent";
 import Projects from "./components/Projects";
+import Service from "./components/Service";
+import Form from "./components/Form";
 
 function App() {
   
@@ -12,6 +14,8 @@ function App() {
         <Header />
         <MainContent/>
         <Projects/>
+        <Service/>
+        <Form/>
 
 
     
---
+++ src/components/Form.jsx
@@ -0,0 +1,113 @@
+import  { useState } from 'react';
+
+const Form = () => {
+  const [datos, setDatos] = useState({
+    nombre: '',
+    email: '',
+    mensaje: '',
+    telefono: ''
+  });
+  const [alerta, setAlerta] = useState(null);
+
+  const handleChange = (e) => {
+    setDatos({
+      ...datos,
+      [e.target.id]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { nombre, email, mensaje, telefono } = datos;
+    if (nombre === '' || email === '' || mensaje === '' || telefono === '') {
+      mostrarAlerta('Todos los campos son obligatorios', true);
+      return;
+    }
+
+    // Lógica para enviar el formulario aquí...
+
+    mostrarAlerta('Mensaje enviado correctamente');
+  };
+
+  const mostrarAlerta = (mensaje, error = false) => {
+    setAlerta({ mensaje, error });
+
+    setTimeout(() => {
+      setAlerta(null);
+    }, 3000);
+  };
+
+  return (
+    <div>
+      <h2 className="centrar-texto">Meet</h2>
+      <form className="formulario" onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Contact us by filling out these fields</legend>
+
+          <div className="contenedor-campos">
+            <div className="campo">
+              <label htmlFor="nombre">Name</label>
+              <input
+                type="text"
+                className="input-text"
+                placeholder="Tu Nombre"
+                id="nombre"
+                value={datos.nombre}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="campo">
+              <label htmlFor="telefono">Phone</label>
+              <input
+                type="tel"
+                className="input-text"
+                id="telefono"
+                placeholder="Tu Teléfono"
+                value={datos.telefono}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="campo">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                className="input-text"
+                id="email"
+                placeholder="Tu correo"
+                value={datos.email}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="campo">
+              <label htmlFor="mensaje">Message</label>
+              <textarea
+                className="input-text"
+                id="mensaje"
+                cols="30"
+                rows="10"
+                value={datos.mensaje}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+          </div>
+          <div className="alinear-derecha flex">
+            <input type="submit" className="boton w-sm-100" value="Send" />
+          </div>
+        </fieldset>
+      </form>
+
+      {/* Renderizar la alerta si existe */}
+      {alerta && (
+        <div className={`alerta ${alerta.error ? 'error' : 'correcto'}`}>
+          {alerta.mensaje}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Form;
